4356de62c789a63554e550e83bfd37a686322ae8
Pooja <heerapooja13@gmail.com>
2022-11-27 17:15:44 +0530

    first commit

---
+++ app.js
@@ -0,0 +1,27 @@
+const hr=document.getElementById("hour");
+const min=document.getElementById("minute");
+const sec=document.getElementById("second");
+const ampmEl=document.getElementById("ampm");
+
+function updateClock(){
+    let h=new Date().getHours();
+    let m=new Date().getMinutes();
+    let s=new Date().getSeconds();
+    let ampm="AM";
+
+    if (h>12) {
+        h -= 12;
+        ampm="PM";
+    }
+    h= h<10? '0'+h : h;
+    m= m<10? '0'+m : m;
+    s= s<10? '0'+s : s;
+    hr.innerText = h;
+    min.innerText = m;
+    sec.innerText = s;
+    ampmEl,(innerText = ampm);
+    setTimeout(()=>{
+        updateClock()
+    },1000)
+}
+updateClock();
